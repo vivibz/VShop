2081cb17ab45dcd43a7f96b13de3f2baf4927058
Viviane Matias <vivianematiasbz00@gmail.com>
2023-06-26 09:18:28 -0300

    initial implementation IdentityServer and migration for tables

---
+++ VShop.IdentityServer/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace VShop.IdentityServer.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {}
+    }
+}
--- VShop.IdentityServer/Program.cs
+++ VShop.IdentityServer/Program.cs
@@ -1,7 +1,38 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using VShop.IdentityServer.Configuration;
+using VShop.IdentityServer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseMySql(mySqlConnection,
+                    ServerVersion.AutoDetect(mySqlConnection)));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+var builderIdentityServer = builder.Services.AddIdentityServer(options =>
+               {
+                options.Events.RaiseErrorEvents = true; //configurando os eventos 
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+                options.EmitStaticAudienceClaim = true;
+              }).AddInMemoryIdentityResources(
+                         IdentityConfiguration.IdentityResources)
+                          .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+                          .AddInMemoryClients(IdentityConfiguration.Clients)
+                          .AddAspNetIdentity<ApplicationUser>();
+
+builderIdentityServer.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -17,7 +48,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
